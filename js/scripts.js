f1b0de35077e76f66685ebeb0f613858fb9d8056
Anna Lawley and paul Hess <alawley1@gmail.com>
2016-01-12 12:01:46 -0800

    add html framework

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = ("input#name").val();
+    var dateInput = ("input#date").val();
+    var startInput = ("input#start-time").val();
+    var endInput = ("input#end-time").val();
+
+    $('.name-span').text(nameInput);
+    $('.date-span').text(dateInput);
+    $('.start').text(startInput);
+    $('.end').text(endInput);
+
+    $('#apptConfirm').show();
+
+    event.preventDefault();
+
+  });
+});
