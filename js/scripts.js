44b63493491e4b7c5f76780f4aad64fd7651283d
Anna Lawley and paul Hess <alawley1@gmail.com>
2016-01-12 12:55:04 -0800

    add jQuery functionality to input form

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var nameInput = ("input#name").val();
-    var dateInput = ("input#date").val();
-    var startInput = ("input#start-time").val();
-    var endInput = ("input#end-time").val();
+    var nameInput = $("input#name").val();
+    var dateInput = $("input#date").val();
+    var startInput = $("input#start-time").val();
+    var endInput = $("input#end-time").val();
 
     $('.name-span').text(nameInput);
     $('.date-span').text(dateInput);
